279b808db186af4a51d278bab473396eb2f7b922
Endah F N <76398931+Fiindah@users.noreply.github.com>
2024-02-08 23:14:36 +0700

    Update first-post.js

--- pages/blogs/first-post.js
+++ pages/blogs/first-post.js
@@ -7,7 +7,7 @@ export default function FirstPost() {
   <>
   <Header />
     <Layout>
-      <h1>Vulgivagus minima creo sunt vis animadverto degenero inflammatio campana vinco qui defendo territo aqua perferendis voluptas.</h1>
+      <h1>Minima creo sunt vis animadverto degenero inflammatio campana vinco qui defendo territo aqua perferendis voluptas.</h1>
       <p>
       Sum triumphus ager. Illum absorbeo ante. Accendo ventosus cedo. Aranea vobis admitto. Baiulus possimus deleo. Solum ultio cimentarius. Excepturi sit cena. Bis tamdiu animi. Amaritudo quibusdam claro. Thymbra in desipio. Traho versus cogo. Ascit corrupti modi. Cras conculco arma. Alo tener desino. Claustrum vereor colligo.
       </p>
