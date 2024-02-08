e927803dda4f3a2e4cd36f2cf13ef7f94cc57dea
Endah F N <76398931+Fiindah@users.noreply.github.com>
2024-02-08 23:51:03 +0700

    Update six-post.js

--- pages/blogs/six-post.js
+++ pages/blogs/six-post.js
@@ -6,10 +6,9 @@ export default function SixPost() {
     <>
     <Header />
     <Layout>
-      <h1>Recusandae absconditus conforto eum incidunt consectetur caveo trans aliqua perspiciatis vultuosus subvenio et peccatus causa vos suppellex.
-      </h1>
+      <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h1>
       <p>
-        Cruciamentum explicabo voluptas. Canis tutamen tergo. Aureus studio anser. Cursus sed ustilo. Amet vetus alius. Currus voluptate adimpleo. Sunt delego annus. Casus textus adnuo. Celo apto deduco. Contego ambitus repellat. Supplanto copiose aeneus. Voveo bestia ademptio. Eos ventus vis. Aestivus amoveo ancilla. Appello color abstergo.
+    Quisque id diam vel quam elementum pulvinar etiam non quam. Dui sapien eget mi proin sed libero enim sed faucibus. Pretium viverra suspendisse potenti nullam. Fringilla est ullamcorper eget nulla facilisi. Odio tempor orci dapibus ultrices in iaculis nunc. Laoreet id donec ultrices tincidunt arcu non sodales. Sit amet venenatis urna cursus eget nunc. A cras semper auctor neque vitae tempus quam. Arcu odio ut sem nulla. Sagittis vitae et leo duis ut diam quam nulla porttitor. Metus vulputate eu scelerisque felis imperdiet proin fermentum leo vel. Ut ornare lectus sit amet est placerat in egestas erat. Nisi est sit amet facilisis magna etiam tempor orci. Fringilla phasellus faucibus scelerisque eleifend donec pretium.
       </p>
       </Layout>
     </>
