6f7d698f687ffcac18d230ddde488632e952a3e5
Endah F N <76398931+Fiindah@users.noreply.github.com>
2024-02-08 23:49:01 +0700

    Update five-post.js

--- pages/blogs/five-post.js
+++ pages/blogs/five-post.js
@@ -6,10 +6,9 @@ export default function FivePost() {
     <>
     <Header />
     <Layout>
-      <h1>Aggredior spiritus tenax aut quod thermae consequatur vobis ager censura apparatus adduco alias solio.
-      </h1>
+      <h1>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h1>
       <p>
-        Comptus aut deputo. Subito demum texo. Balbus cibo tamen. Ut admoveo attonbitus. Centum modi basium. Vulticulus degenero canto. Est congregatio astrum. Desparatus cervus certe. Unde coma caste. Totus aegrus abbas. Demens eius tutis. Vilitas at vito. Pauper aggero ultio. Callide vix ciminatio. Arx spectaculum cunabula. Toties libero tot. Cura virga omnis. Totidem strenuus animus.
+    Nunc congue nisi vitae suscipit tellus mauris a. Nibh tortor id aliquet lectus proin nibh. Rutrum quisque non tellus orci ac. Tempus quam pellentesque nec nam aliquam sem et tortor. Malesuada fames ac turpis egestas maecenas pharetra. Tortor id aliquet lectus proin nibh nisl. Ut placerat orci nulla pellentesque dignissim enim sit amet. Tincidunt lobortis feugiat vivamus at augue. Pellentesque nec nam aliquam sem et. Sed odio morbi quis commodo. In vitae turpis massa sed elementum tempus egestas. Pharetra vel turpis nunc eget lorem dolor sed. Tincidunt dui ut ornare lectus sit amet. Amet justo donec enim diam vulputate ut pharetra sit. Fames ac turpis egestas maecenas pharetra. Sociis natoque penatibus et magnis dis parturient montes.
       </p>
       </Layout>
     </>
