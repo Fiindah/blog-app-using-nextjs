4113e1ee98d290a3167d08a375248bfe60beec39
Endah F N <76398931+Fiindah@users.noreply.github.com>
2024-02-09 00:04:50 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -18,57 +18,57 @@ export default function Homepage() {
         <div className={styles.grid}>
           <div className={styles.card}>
             <Link href="/blogs/first-post">
-              <h3>Vulgivagus minima creo sunt vis animadverto degenero inflammatio campana vinco qui defendo territo aqua perferendis voluptas.</h3>
+              <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h3>
             </Link>
             <p>
-            Sum triumphus ager. Illum absorbeo ante. Accendo ventosus cedo. Aranea vobis admitto. Baiulus possimus deleo. Solum ultio cimentarius. Excepturi sit cena. Bis tamdiu animi....
+              In arcu cursus euismod quis. Vitae et leo duis ut diam. Ipsum nunc aliquet bibendum enim facilisis gravida. Donec et odio pellentesque diam volutpat commodo sed. Aliquet risus feugiat in ante metus dictum at tempor. Quam lacus suspendisse faucibus interdum posuere. Scelerisque eleifend donec pretium vulputate sapien. Quis ipsum suspendisse ultrices gravida dictum. Facilisis mauris sit amet massa vitae tortor condimentum. Ac ut consequat semper viverra nam libero justo. Fermentum leo vel orci porta non pulvinar neque...
             </p>
           </div>
           <div className={styles.card}>
             <Link href="/blogs/second-post">
-              <h3>Aperio deficio sum illum eum defleo summopere error totus.</h3>
+              <h3>Adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h3>
             </Link>
             <p>
-            Molestiae denuo cuppedia. Certo cetera ambitus. Vilis vae utique. Apparatus curriculum viscus. Architecto currus triduana...
+            Pharetra magna ac placerat vestibulum lectus mauris ultrices eros. Nec sagittis aliquam malesuada bibendum. Nibh cras pulvinar mattis nunc sed. Sodales ut eu sem integer vitae. Orci phasellus egestas tellus rutrum tellus pellentesque. Velit euismod in pellentesque massa. Varius morbi enim nunc faucibus a pellentesque sit amet. Aenean sed adipiscing diam donec adipiscing tristique risus nec feugiat. Diam quis enim lobortis scelerisque fermentum dui faucibus. Euismod nisi porta lorem mollis aliquam ut...
             </p>
           </div>
           <div className={styles.card}>
             <Link href="/blogs/three-post">
-              <h3>Dedecor vitiosus utilis commodo sursum officia quo decor non vesper.</h3>
+              <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h3>
             </Link>
             <p>
-            Recusandae sint canonicus. Decimus speciosus contego. Civis vesper cultellus. Vulpes cursim corporis. Vestigium substantia spero...
+              Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Iaculis eu non diam phasellus vestibulum. Sit amet porttitor eget dolor morbi non. Sed sed risus pretium quam vulputate dignissim. Ipsum nunc aliquet bibendum enim facilisis gravida neque convallis a. Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla. Amet luctus venenatis lectus magna fringilla urna porttitor rhoncus. Elit pellentesque habitant morbi tristique senectus et. ..
             </p>
           </div>
           <div className={styles.card}>
             <Link href="/blogs/four-post">
-              <h3>Casso vester harum vesica stillicidium cupressus cognatus cubo aer vel constans id audio chirographum.</h3>
+              <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h3>
             </Link>
             <p>
-            Dolores acquiro alo. Texo absum tempora. Cerno victus tolero. Crepusculum comedo infit. Blandior consequatur cavus. Crepusculum velit accipio...
+            Dignissim convallis aenean et tortor at risus. Diam quis enim lobortis scelerisque fermentum dui faucibus in ornare. Amet est placerat in egestas erat imperdiet sed. Diam sollicitudin tempor id eu nisl nunc mi ipsum faucibus. Sed felis eget velit aliquet sagittis. Aliquet lectus proin nibh nisl condimentum id venenatis a. Odio euismod lacinia at quis risus sed vulputate. Amet volutpat consequat mauris nunc. Ut tristique et egestas quis. Ante metus dictum at tempor commodo ullamc...
             </p>
           </div>
           <div className={styles.card}>
             <Link href="/blogs/five-post">
-              <h3>Aggredior spiritus tenax aut quod thermae consequatur vobis ager censura apparatus adduco alias solio.</h3>
+              <h3>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h3>
             </Link>
             <p>
-            Comptus aut deputo. Subito demum texo. Balbus cibo tamen. Ut admoveo attonbitus. Centum modi basium. Vulticulus degenero canto. Est congregatio...
+            Nunc congue nisi vitae suscipit tellus mauris a. Nibh tortor id aliquet lectus proin nibh. Rutrum quisque non tellus orci ac....
             </p>
           </div>
           <div className={styles.card}>
             <Link href="/blogs/six-post">
-              <h3>Recusandae absconditus conforto eum incidunt consectetur caveo trans aliqua perspiciatis vultuosus subvenio et peccatus causa vos suppellex.</h3>
+              <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h3>
             </Link>
             <p>
-            Cruciamentum explicabo voluptas. Canis tutamen tergo. Aureus studio anser. Cursus sed ustilo. Amet vetus alius. Currus voluptate adimpleo. Sunt...
+            Quisque id diam vel quam elementum pulvinar etiam non quam. Dui sapien eget mi proin sed libero enim sed faucibus....
             </p>
           </div>
         </div>
         </main>
 
       <footer className={styles.footer}>
-        <p>Copyright 2023 Stories Bar by Endah FN</p>
+        <p>Copyright 2024 Stories Bar by Endah FN</p>
       </footer>
     </div>
   );
