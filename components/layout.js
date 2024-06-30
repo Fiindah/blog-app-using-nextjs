faa2c5cb2802195c2fd2f90f088034dbc2dc1b21
Endah F N <76398931+Fiindah@users.noreply.github.com>
2024-06-30 22:01:48 +0700

    Delete components directory

--- components/layout.js
+++
@@ -1,6 +0,0 @@
-import styles from "./layout.module.css";
-// import Header from "./header";
-
-export default function Layout({ children }) {
-  return <div className={styles.container}>{children}</div>
-}
