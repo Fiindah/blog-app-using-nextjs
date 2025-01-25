136e445f7637a441b9ae07d84e84590bb8f0bfb8
Endah F N <76398931+Fiindah@users.noreply.github.com>
2025-01-25 09:22:11 +0700

    Update hello.js
    update data api

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
-}
+import axios from 'axios';
+
+export const fetchFoodData = async () => {
+  const response = await axios.get('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+  return response.data.meals;
+};
