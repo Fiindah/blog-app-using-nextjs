fdda8fd17b6a0d93e333762297ac8dae03fb06c0
Endah F N <76398931+Fiindah@users.noreply.github.com>
2025-01-25 14:10:04 +0700

    Delete pages directory

--- pages/api/hello.js
+++
@@ -1,8 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-import axios from 'axios';
-
-export const fetchFoodData = async () => {
-  const response = await axios.get('https://www.themealdb.com/api/json/v1/1/search.php?s=');
-  return response.data.meals;
-};
